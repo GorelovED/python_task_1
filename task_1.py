fbec41ab7b911299f1436270978a3770b8e2cdd0
acer-pc-evgeniy <e.v.gen.gorelov@yandex.ru>
2022-12-01 19:34:22 +0400

    Решение первой задачи

---
+++ task_1.py
@@ -0,0 +1,8 @@
+numbers = int(input("Введите число от 1 до 7"))
+if numbers < 1 or  numbers > 7 :
+    print ("Вы ввели не правильное число попробуйте ещё раз")
+elif numbers > 5:
+    print ("Ура!Это выходной!")
+elif numbers:
+    print ("Увы нужно ещё поработать")
+    
